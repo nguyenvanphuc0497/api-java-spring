9d654c392acb4f564a095eb7436c714a18e38784
Nguyen Van Phuc <nguyenvanphuc0497@MBA0123.local>
2018-09-18 20:22:55 +0700

    first commit

---
+++ src/main/kotlin/com/capstone1/tutoryapi/TutoryApiApplication.kt
@@ -0,0 +1,11 @@
+package com.capstone1.tutoryapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TutoryApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<TutoryApiApplication>(*args)
+}
