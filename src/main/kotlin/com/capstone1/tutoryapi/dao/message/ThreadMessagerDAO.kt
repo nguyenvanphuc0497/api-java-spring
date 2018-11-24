c57c96b3252e06e9cab3b6ba4f498403bb492c1d
Nguyen Van Phuc <nguyenvanphuc0497@MBA0123.local>
2018-11-24 12:25:09 +0700

    fix query select thread  get info profile not correct

--- src/main/kotlin/com/capstone1/tutoryapi/dao/message/ThreadMessagerDAO.kt
+++ src/main/kotlin/com/capstone1/tutoryapi/dao/message/ThreadMessagerDAO.kt
@@ -12,10 +12,17 @@ import org.springframework.stereotype.Repository
 class ThreadMessagerDAO : BaseDAO() {
 
     internal fun getProfileForThread(idProfile: Int?): List<ThreadMessage> {
-        val sql = "SELECT tm.*, up.NAME AS NAME_RECEIVER , up.URL_AVATAR AS URL_AVATAR_RECEIVER " +
-                "FROM thread_messager AS tm INNER JOIN user_profile AS up " +
-                "WHERE tm.RECEIVER_IDPROFILE = up.ID_PROFILE AND( " +
-                "tm.SENDER_IDPROFILE = $idProfile OR tm.RECEIVER_IDPROFILE = $idProfile ) "
+//        val sql = "SELECT tm.*, up.NAME AS NAME_RECEIVER , up.URL_AVATAR AS URL_AVATAR_RECEIVER " +
+//                "FROM thread_messager AS tm INNER JOIN user_profile AS up " +
+//                "WHERE tm.RECEIVER_IDPROFILE = up.ID_PROFILE AND( " +
+//                "tm.SENDER_IDPROFILE = $idProfile OR tm.RECEIVER_IDPROFILE = $idProfile ) "
+        //Fix error get info profile not correct
+        val sql = "SELECT tm.*, up.NAME AS NAME_RECEIVER, up.URL_AVATAR AS URL_AVATAR_RECEIVER " +
+                "FROM thread_messager AS tm INNER JOIN user_profile AS up ON tm.RECEIVER_IDPROFILE = up.ID_PROFILE " +
+                "WHERE tm.SENDER_IDPROFILE = $idProfile " +
+                "UNION SELECT tm.*, up.NAME AS NAME_RECEIVER, up.URL_AVATAR AS URL_AVATAR_RECEIVER " +
+                "FROM thread_messager AS tm INNER JOIN user_profile AS up ON tm.SENDER_IDPROFILE = up.ID_PROFILE " +
+                "WHERE tm.RECEIVER_IDPROFILE = $idProfile "
         return jdbcTemplate.query(sql, ThreadMessageMapper())
     }
 }
