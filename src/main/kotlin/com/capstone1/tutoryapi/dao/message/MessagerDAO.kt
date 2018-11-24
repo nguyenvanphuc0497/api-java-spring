113a9d1d8381da4561976623d937ae397e212116
Nguyen Van Phuc <nguyenvanphuc0497@MBA0123.local>
2018-11-24 10:54:46 +0700

    fix sending message api

--- src/main/kotlin/com/capstone1/tutoryapi/dao/message/MessagerDAO.kt
+++ src/main/kotlin/com/capstone1/tutoryapi/dao/message/MessagerDAO.kt
@@ -3,7 +3,6 @@ package com.capstone1.tutoryapi.dao.message
 import com.capstone1.tutoryapi.dao.BaseDAO
 import com.capstone1.tutoryapi.entities.EntitiesTable
 import com.capstone1.tutoryapi.entities.messager.MessagerMapper
-import com.capstone1.tutoryapi.entities.messager.ThreadMessageMapper
 import org.springframework.stereotype.Repository
 
 /**
@@ -15,13 +14,23 @@ class MessagerDAO : BaseDAO() {
             "SELECT * FROM ${EntitiesTable.message} WHERE ID_THREAD = $idThread", MessagerMapper())
 
     internal fun createMessageByIdThread(idProfile: Int?, idThread: Int?, message: String?): Int {
-        val count = jdbcTemplate.query("SELECT * FROM ${EntitiesTable.threadMessage} WHERE ID_THREAD = $idThread AND SENDER_IDPROFILE =$idProfile", ThreadMessageMapper()).size
+        var count: Int? = 0
+        jdbcTemplate.query("SELECT * FROM ${EntitiesTable.threadMessage} WHERE ID_THREAD = $idThread AND SENDER_IDPROFILE =$idProfile") {
+            it.row.let { row ->
+                count = row
+            }
+        }
         val isSender = if (count == 1) {
             1
         } else {
             0
         }
 
-        return jdbcTemplate.update("INSERT INTO `${EntitiesTable.message}` (`ID_THREAD`, `MESSAGECOL`, `IS_SENDER`) VALUES ('$idThread', '$message', '$isSender')")
+        val sql = "INSERT INTO `${EntitiesTable.message}` (`ID_THREAD`, `MESSAGECOL`, `IS_SENDER`) VALUES ('$idThread', '$message', '$isSender')"
+//        Kiem tra su ton tai cua nguoi do hay khong trong phong chat
+//        val sql = "INSERT INTO `${EntitiesTable.message}` (`ID_THREAD`, `MESSAGECOL`, `IS_SENDER`) " +
+//                " VALUES ('$idThread', '$message', '$isSender') " +
+//                "WHERE EXITST( SELECT * FROM ${EntitiesTable.threadMessage} WHERE SENDER_IDPROFILE = $idProfile OR RECEIVER_IDPROFILE = $idProfile )"
+        return jdbcTemplate.update(sql)
     }
 }
