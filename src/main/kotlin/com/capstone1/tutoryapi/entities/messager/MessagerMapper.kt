829d6452085362814f68afba1ae87b6e52f87de1
Nguyen Van Phuc <nguyenvanphuc0497@MBA0123.local>
2018-12-15 11:11:03 +0700

    update api message time send

--- src/main/kotlin/com/capstone1/tutoryapi/entities/messager/MessagerMapper.kt
+++ src/main/kotlin/com/capstone1/tutoryapi/entities/messager/MessagerMapper.kt
@@ -11,6 +11,7 @@ class MessagerMapper : RowMapper<Messager> {
             rs.getInt("ID_THREAD"),
             rs.getString("IS_SENDER"),
             rs.getString("MESSAGECOL"),
+            rs.getString("SEND_TIME"),
             rs.getString("READER"),
             rs.getString("TIME_READ"),
             rs.getString("SENDER_IDPROFILE"))
