dc3d03a979e459d2760156275065d3ee2e3cf76b
Nguyen Van Phuc <mba0123@MBA0123.local>
2018-12-22 19:39:04 +0700

    create api load post by id profile

--- src/main/kotlin/com/capstone1/tutoryapi/controller/news/PostsController.kt
+++ src/main/kotlin/com/capstone1/tutoryapi/controller/news/PostsController.kt
@@ -1,7 +1,9 @@
 package com.capstone1.tutoryapi.controller.news
 
 import com.capstone1.tutoryapi.controller.BaseController
+import org.springframework.data.jpa.repository.Query
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 /**
@@ -13,4 +15,6 @@ class PostsController : BaseController() {
     @GetMapping("/post")
     internal fun viewPost() = PostsResponse(postsDAO.selectAddPostAvailbel())
 
+    @GetMapping("/post/")
+    internal fun viewPostByIdProfile(@RequestParam("id") idProfile: String?) = PostsResponse(postsDAO.selectPostAvailbelByIdProfile(idProfile))
 }
--- src/main/kotlin/com/capstone1/tutoryapi/dao/posts/PostsDAO.kt
+++ src/main/kotlin/com/capstone1/tutoryapi/dao/posts/PostsDAO.kt
@@ -15,12 +15,21 @@ class PostsDAO : BaseDAO() {
     internal fun viewPost() = jdbcTemplate.query(
             "SELECT * FROM ${EntitiesTable.postTable}", PostMapper())
 
-    internal fun selectAddPostAvailbel(): List<Post> {
+    internal fun selectAddPostAvailbel(): List<Post>? {
         val sql = "SELECT uf.ID_PROFILE, n.NEWS_ID,uf.URL_AVATAR, n.NEWS_TITLE, n.DETAILS, en.FROM_DATE " +
                 "FROM news n " +
                 "INNER JOIN editor_of_news en ON en.THING_ROLE_TYPE_ID_TO=n.NEWS_ID " +
                 "INNER JOIN editor ed ON ed.PARTY_ID=en.PARTY_ROLE_TYPE_ID_FROM INNER JOIN user_profile uf ON uf.ID_USER= ed.ID_USER " +
                 "WHERE n.STATUS= 1 ORDER BY en.FROM_DATE DESC "
+        return jdbcTemplate.query(sql, PostMapper())
+    }
+
+    internal fun selectPostAvailbelByIdProfile(idProfile: String?): List<Post>? {
+        val sql = "SELECT uf.ID_PROFILE, n.NEWS_ID,uf.URL_AVATAR, n.NEWS_TITLE, n.DETAILS, en.FROM_DATE " +
+                "FROM news n " +
+                "INNER JOIN editor_of_news en ON en.THING_ROLE_TYPE_ID_TO=n.NEWS_ID " +
+                "INNER JOIN editor ed ON ed.PARTY_ID=en.PARTY_ROLE_TYPE_ID_FROM INNER JOIN user_profile uf ON uf.ID_USER= ed.ID_USER " +
+                "WHERE uf.ID_PROFILE= '$idProfile' ORDER BY en.FROM_DATE DESC "
         return jdbcTemplate.query(sql, PostMapper())
     }
 }
